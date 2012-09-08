3e1a55e3c678c8a84cb4b56333a4befa21a60c07
dvputnam <putnamd@smccd.edu>
2012-09-07 21:01:07 -0700

    Removed long explanation from my.py and moved it to README>md

--- my.py
+++ my.py
@@ -5,102 +5,9 @@
 # Date: Tue Sep  4 19:05:57 PDT 2012
 
 """ 
-The my.py framework 
 
-Use this mini web framework to reduce some of the repetitive and non-productive
-aspects of working with HTML. This little script does one thing to make your
-life better: it creates a simple templating system that allows you to use the 
-same HTML template for many pages. 
+Before operating this machinery, read the README.md file.
 
-    1) You create the HTML templates.
-    2) my.py inserts the dynamic content into the template.
-    3) You can have multiple templates and specify them in the URL.
-    4) Templates and dynamic pages are kept out of the web directory and are
-       not directly accessible via a browser.
-
-TYPICAL URL:
-
-    http://csmcis2.csmcis.com/~YOURNAME/my.py/lab2.1
-
-  How It Works -
-
-    1) my.py parses the URL and sees that you want to display lab2.1.html 
-       (notice the .html was omitted for prettiness)
-    2) my.py finds lab2.1.html and inserts it into a "template" which you
-       can use with all of your HTML pages.
-    3) By using a template, you can change the look of all of your pages by
-       simply changing the HTML in the template, and all of your pages will
-       inherit the changes.
-
-TYPICAL URL WITH SPECIFIC TEMPLATE:
-
-    http://cmscis2.csmcis.com/~YOURNAME/my.py/lab2.1/gothic
-
-  How It Works -
-
-    1) my.py sees that you want to insert the contents of lab2.1.html into the 
-       your special goth template that you think is cool.
-    2) This means that you have a template named gothic.html in the layouts
-       directory.
-
-OTHER FEATURES:
-
-    1) If you request a non-existent layout, you get the default layout.
-    2) If you request a non-existent HTML page, you get your customized error404.html page.
-    3) No one can view any of your pages directly: everything goes through my.py
-    4) All requests are sanitized so that no one can mess with you.
-
-REQUIREMENTS:
-
-    To make this all work, you need to create some directories and some default
-    files:
-
-        DIRECTORIES:
-
-          /home/students/YOURNAME/assets/cis127/layouts/
-          /home/students/YOURNAME/assets/cis127/html5/
-
-        FILES: 
-
-          /home/students/YOURNAME/assets/cis127/layouts/default.html
-          /home/students/YOURNAME/assets/cis127/html5/index.html
-          /home/students/YOURNAME/assets/cis127/html5/error404.html
-
-EXAMPLE MINIMAL default.html LAYOUT:
-
-    You can make your layouts as elaborate as you wish, or as simple.
-
-    1) {title} will be filled in automatically
-    2) {content} will be the HTML page in the URL
-
-        <!doctype html>
-        <html lang=en>
-        <head>
-          <meta charset="utf-8">
-          <title>%s</title> 
-        </head>
-        {content}
-
-       
-EXAMPLE MINIMAL HTML PAGE
-    The title in the comment will be inserted into the HEAD tag. 
-    
-        <!-- title="My Beautiful Web Page" -->
-        <h1>Hello, world</h1>
-
-KNOWN ISSUES
-
-    my.py is written in Python3. If you want to print "{}"" characters,
-    as in an embedded stylesheet, you have to double up the braces, or
-    you will have fatal errors:
-
-        BAD: p {background-color:blue}
-        
-        GOOD: p {{background-color:blue}}
-
-Keep hacking...
-
-Doug Putnam
 
 """
 #############  my.py at work #################
@@ -187,7 +94,7 @@ except IOError:
 
 page = DEFAULT_PAGE
 
-message ="The page you are looking for is not available."
+message404 ="The page you are looking for is not available."
 
 if 'PATH_INFO' in os.environ.keys():
     parts = os.environ['PATH_INFO'].split('/')
@@ -196,7 +103,7 @@ if 'PATH_INFO' in os.environ.keys():
         page = cgi.escape(parts[0]) + '.html'
         if page == 'sitemap.html':
             page="error404.html"
-            message = 'The sitemap is not available.'
+            message404 = 'The sitemap is not available.'
 
         if not os.path.isfile(HTML5 + page):
             page = ERROR_404
@@ -209,7 +116,7 @@ if 'PATH_INFO' in os.environ.keys():
 layout = open(layout_path,'r').read()
 content = open(HTML5 + page,'r').read()
 if page == 'error404.html':
-    content = content.format(content=message)
+    content = content.format(content=message404)
 
 res = re.search('<!--\s*title="?([^"]*)"?\s*-->',content)
 title = page
